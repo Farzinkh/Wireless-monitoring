2a452e8623a058777a4c7120e3b4564efb89960d
Farzinkh <farzin.mag@gmail.com>
2021-04-21 13:57:55 +0430

    set pc as server and esp as client and modifie example connection with my wifi default settings

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 
-# UDP Server example
+# UDP Client example
 
 (See the README.md file in the upper level 'examples' directory for more information about examples.)
 
-The application creates UDP socket with the specified port number and waits for the data to be received. Received data are printed as ASCII text and retransmitted back to the client.
+The application creates UDP socket and sends message to the predefined port and IP address. After the server's reply, the application prints received reply as ASCII text, waits for 2 seconds and sends another message.
 
 ## How to use example
 
-In order to create UDP client that communicates with UDP server example, choose one of the following options.
+In order to create UDP server that communicates with UDP Client example, choose one of the following options.
 
 There are many host-side tools which can be used to interact with the UDP/TCP server/client. 
 One command line tool is [netcat](http://netcat.sourceforge.net) which can send and receive many kinds of packets. 
@@ -25,20 +25,20 @@ echo "Hello from PC" | nc -w1 -u 192.168.0.167 3333
 echo "Hello from PC" | nc -w1 -u 192.168.0.167 3333
 ```
 
-### UDP client using netcat
+### UDP server using netcat
 ```
-nc -u 192.168.0.167 3333
+nc -u -l 192.168.0.167 3333
 ```
 
 ### Python scripts
-Script example_test.py could be used as a counter part to the udp-server application,
-IP address and the message to be send to the server shall be stated as arguments. Example:
+Script example_test.py could be used as a counter part to the udp-client application, ip protocol name (IPv4 or IPv6) shall be stated as argument. Example:
 
 ```
-python example_test.py 192.168.0.167 Message
+python example_test.py IPv4
 ```
 Note that this script is used in automated tests, as well, so the IDF test framework packages need to be imported;
 please add `$IDF_PATH/tools/ci/python_packages` to `PYTHONPATH`.
+
 
 ## Hardware Required
 
@@ -52,11 +52,16 @@ idf.py menuconfig
 
 Set following parameters under Example Configuration Options:
 
-* Set `IP version` of the example to be IPV4 or IPV6.
+* Set `IP version` of example to be IPV4 or IPV6.
 
-* Set `Port` number that represents remote port the example will create.
+* Set `IPV4 Address` in case your chose IP version IPV4 above.
+
+* Set `IPV6 Address` in case your chose IP version IPV6 above.
+
+* Set `Port` number that represents remote port the example will send data and receive data from.
 
 Configure Wi-Fi or Ethernet under "Example Connection Configuration" menu. See "Establishing Wi-Fi or Ethernet Connection" section in [examples/protocols/README.md](../../README.md) for more details.
+
 
 ## Build and Flash
 
--- example_test.py
+++ example_test.py
@@ -12,7 +12,10 @@ import os
 import re
 import socket
 import sys
+from builtins import input
+from threading import Event, Thread
 
+import netifaces
 import ttfw_idf
 
 # -----------  Config  ----------
@@ -21,43 +24,73 @@ INTERFACE = 'eth0'
 # -------------------------------
 
 
-def udp_client(address, payload):
-    for res in socket.getaddrinfo(address, PORT, socket.AF_UNSPEC,
-                                  socket.SOCK_DGRAM, 0, socket.AI_PASSIVE):
-        family_addr, socktype, proto, canonname, addr = res
-    try:
-        sock = socket.socket(family_addr, socket.SOCK_DGRAM)
-        sock.settimeout(60.0)
-    except socket.error as msg:
-        print('Could not create socket: ' + str(msg[0]) + ': ' + msg[1])
-        raise
-    try:
-        sock.sendto(payload.encode(), addr)
-        reply, addr = sock.recvfrom(128)
-        if not reply:
-            return
-        print('Reply[' + addr[0] + ':' + str(addr[1]) + '] - ' + str(reply))
-    except socket.error as msg:
-        print('Error Code : ' + str(msg[0]) + ' Message: ' + msg[1])
-        sock.close()
-        raise
-    return reply.decode()
+def get_my_ip(type):
+    for i in netifaces.ifaddresses(INTERFACE)[type]:
+        return i['addr'].replace('%{}'.format(INTERFACE), '')
+
+
+class UdpServer:
+
+    def __init__(self, port, family_addr, persist=False):
+        self.port = port
+        self.family_addr = family_addr
+        self.socket = socket.socket(family_addr, socket.SOCK_DGRAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.settimeout(60.0)
+        self.shutdown = Event()
+        self.persist = persist
+
+    def __enter__(self):
+        try:
+            self.socket.bind(('', self.port))
+        except socket.error as e:
+            print('Bind failed:{}'.format(e))
+            raise
+
+        print('Starting server on port={} family_addr={}'.format(self.port, self.family_addr))
+        self.server_thread = Thread(target=self.run_server)
+        self.server_thread.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.persist:
+            sock = socket.socket(self.family_addr, socket.SOCK_DGRAM)
+            sock.sendto(b'Stop', ('localhost', self.port))
+            sock.close()
+            self.shutdown.set()
+        self.server_thread.join()
+        self.socket.close()
+
+    def run_server(self):
+        while not self.shutdown.is_set():
+            try:
+                data, addr = self.socket.recvfrom(1024)
+                if not data:
+                    return
+                data = data.decode()
+                print('Reply[' + addr[0] + ':' + str(addr[1]) + '] - ' + data)
+                reply = 'OK: ' + data
+                self.socket.sendto(reply.encode(), addr)
+            except socket.error as e:
+                print('Running server failed:{}'.format(e))
+                raise
+            if not self.persist:
+                break
 
 
 @ttfw_idf.idf_example_test(env_tag='Example_WIFI')
-def test_examples_protocol_socket_udpserver(env, extra_data):
-    MESSAGE = 'Data to ESP'
+def test_examples_protocol_socket_udpclient(env, extra_data):
     """
     steps:
       1. join AP
       2. have the board connect to the server
       3. send and receive data
     """
-    dut1 = env.get_dut('udp_server', 'examples/protocols/sockets/udp_server', dut_class=ttfw_idf.ESP32DUT)
+    dut1 = env.get_dut('udp_client', 'examples/protocols/sockets/udp_client', dut_class=ttfw_idf.ESP32DUT)
     # check and log bin size
-    binary_file = os.path.join(dut1.app.binary_path, 'udp_server.bin')
+    binary_file = os.path.join(dut1.app.binary_path, 'udp_client.bin')
     bin_size = os.path.getsize(binary_file)
-    ttfw_idf.log_performance('udp_server_bin_size', '{}KB'.format(bin_size // 1024))
+    ttfw_idf.log_performance('udp_client_bin_size', '{}KB'.format(bin_size // 1024))
 
     # start test
     dut1.start_app()
@@ -68,20 +101,24 @@ def test_examples_protocol_socket_udpserver(env, extra_data):
     print('Connected with IPv4={} and IPv6={}'.format(ipv4, ipv6))
 
     # test IPv4
-    received = udp_client(ipv4, MESSAGE)
-    if not received == MESSAGE:
-        raise
-    dut1.expect(MESSAGE)
+    with UdpServer(PORT, socket.AF_INET):
+        server_ip = get_my_ip(netifaces.AF_INET)
+        print('Connect udp client to server IP={}'.format(server_ip))
+        dut1.write(server_ip)
+        dut1.expect(re.compile(r'OK: Message from ESP32'))
     # test IPv6
-    received = udp_client('{}%{}'.format(ipv6, INTERFACE), MESSAGE)
-    if not received == MESSAGE:
-        raise
-    dut1.expect(MESSAGE)
+    with UdpServer(PORT, socket.AF_INET6):
+        server_ip = get_my_ip(netifaces.AF_INET6)
+        print('Connect udp client to server IP={}'.format(server_ip))
+        dut1.write(server_ip)
+        dut1.expect(re.compile(r'OK: Message from ESP32'))
 
 
 if __name__ == '__main__':
-    if sys.argv[2:]:    # if two arguments provided:
-        # Usage: example_test.py <server_address> <message_to_send_to_server>
-        udp_client(sys.argv[1], sys.argv[2])
-    else:               # otherwise run standard example test as in the CI
-        test_examples_protocol_socket_udpserver()
+    if sys.argv[1:] and sys.argv[1].startswith('IPv'):     # if additional arguments provided:
+        # Usage: example_test.py <IPv4|IPv6>
+        family_addr = socket.AF_INET6 if sys.argv[1] == 'IPv6' else socket.AF_INET
+        with UdpServer(PORT, family_addr, persist=True) as s:
+            print(input('Press Enter stop the server...'))
+    else:
+        test_examples_protocol_socket_udpclient()
